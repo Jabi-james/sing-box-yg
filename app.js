cd77a31df3c406b91843e1f26e19436271d48c92
Jabi-james <91572300+Jabi-james@users.noreply.github.com>
2025-01-31 15:59:44 +0330

    Update app.js

--- app.js
+++ app.js
@@ -5,29 +5,29 @@ const app = express();
 app.use(express.json());
 const commandToRun = "cd ~ && bash serv00keep.sh";
 function runCustomCommand() {
-    exec(commandToRun, function (err, stdout, stderr) {
-        if (err) {
-            console.log("命令执行错误: " + err);
-            return;
-        }
-        if (stderr) {
-            console.log("命令执行标准错误输出: " + stderr);
-        }
-        console.log("命令执行成功:\n" + stdout);
-    });
+exec(commandToRun, function (err, stdout, stderr) {
+if (err) {
+console.log("Command execution error: " + err);
+return;
 }
-setInterval(runCustomCommand, 3 * 60 * 1000); // 3 分钟 = 3 * 60 * 1000 毫秒
+if (stderr) {
+console.log("Command execution standard error output: " + stderr);
+}
+console.log("Command execution success:\n" + stdout);
+});
+}
+setInterval(runCustomCommand, 3 * 60 * 1000); // 3 minutes = 3 * 60 * 1000 milliseconds
 app.get("/up", function (req, res) {
-    runCustomCommand();
-    res.type("html").send("<pre>Serv00网页保活启动：Serv00！UP！UP！UP！</pre>");
+runCustomCommand();
+res.type("html").send("<pre>Serv00 web page keep alive start: Serv00! UP! UP! UP! </pre>");
 });
 app.use((req, res, next) => {
-    if (req.path === '/up') {
-        return next();
-    }
-    res.status(404).send('把浏览器地址改为：http://name.name.serv00.net/up 这样才能启动Serv00网页保活');
+if (req.path === '/up') {
+return next();
+}
+res.status(404).send('Change the browser address to: http://name.name.serv00.net/up to start Serv00 web page keep alive');
 });
 app.listen(3000, () => {
-    console.log("服务器已启动，监听端口 3000");
-    runCustomCommand();
+console.log("Server started, listening on port 3000");
+runCustomCommand();
 });
